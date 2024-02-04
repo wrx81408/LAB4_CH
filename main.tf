fe206232ebb9a89b6cf6b6c5722e34d2a99e638b
wrx81408 <152698478+wrx81408@users.noreply.github.com>
2024-02-04 15:40:33 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,12 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.30.0"
-    }
-  }
-}
-
 provider "aws" {
-  region  = "eu-west-1"
-}
-
-resource "aws_instance" "MeritoWSB" {
-  ami           = "ami-00526aba2644bba2d"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "MeritoWSB"
+  region                      = "eu-west-1"
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  s3_force_path_style         = true
+  endpoints {
+    ec2 = "http://localhost:4566"
   }
 }
